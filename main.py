1f967366a21fc25cc278711d2f35c4acd2a39246
NiccoloMachiaveli <Koroliskander@yandex.ru>
2025-05-16 20:47:54 +0400

    мини очистка
    игра дальше будет сильно меняться

--- main.py
+++ main.py
@@ -7,22 +7,17 @@ FPS = 30
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 
-# This sets the WIDTH and HEIGHT of each grid location
 WIDTH = 20
 HEIGHT = 20
 
-# This sets the margin between each cell
 MARGIN = 1
 
 grid = []
 
-# вместо этого можно использовать генератор списков
 for row in range(12):
-    # Add an empty array that will hold each cell
-    # in this row
     grid.append([])
     for column in range(12):
-        grid[row].append(0)  # Append a cell
+        grid[row].append(0)
 #grid[5][4] = 1
 grid[5][5] = 1
 #grid[5][6] = 1
@@ -30,14 +25,15 @@ grid[5][5] = 1
 grid[6][6] = 1
 grid[7][4] = 1
 grid[7][5] = 1
-grid[7][6] = 1 #
-# Создаем игру и окно
+grid[7][6] = 1
+
 pygame.init()
 pygame.mixer.init()
 src = pygame.display.set_mode((255, 255))
 pygame.display.set_caption('Game of life')
 clock = pygame.time.Clock()
 neighbours = {}
+
 
 def life(pos):
     new_pos = copy.deepcopy(pos)
@@ -61,8 +57,6 @@ def life(pos):
                     neighbours[(row, column)] = [pos[row - 1][column - 1], pos[row - 1][column], pos[row - 1][column + 1],
                                                  pos[row][column - 1], pos[row][column + 1],
                                                  pos[row + 1][column - 1], pos[row + 1][column], pos[row + 1][column + 1]]
-                '''if neighbours[(row, column)].count(1) == 3 or neighbours[(row, column)].count(1) == 2:
-                    return pos'''
                 if neighbours[(row, column)].count(1) < 2:
                     new_pos[row][column] = 0
                 elif neighbours[(row, column)].count(1) > 3:
